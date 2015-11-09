eff781e356ecc10a343ff22a43e07437b9f2ad9e
Ewetumo Alexander <trinoxf@gmail.com>
2015-11-09 08:18:18 +0100

    added function name and line details to dev

--- logd/logd_test.go
+++ logd/logd_test.go
@@ -4,24 +4,24 @@ import "testing"
 
 func TestDevLog(t *testing.T) {
 
-	// => Level: app.Dev Type:... Time: 2015-02-01 12:32:40:4032 Message: ...
+	// => Level: app.Dev Type:... Time: 2015-02-01 12:32:40:4032 Func: TestDevLog  Line:30:322 Message: ...
 	Dev.Log(struct{}{}, InfoLevel, "TestDevLog", "Write to bridget")
 	Dev.Logf(struct{}{}, InfoLevel, "TestDevLog", "Write to %s", "john")
 
-	// => Level: app.Dev Type: Debug Time: 2015-02-01 12:32:40:4032 Message: ...
+	// => Level: app.Dev Type: Debug Time: 2015-02-01 12:32:40:4032 Func: TestDevLog  Line:30:322  Message: ...
 	Dev.Debug(struct{}{}, "TestDevLog", "Write to sumer")
 	Dev.Debugf(struct{}{}, "TestDevLog", "Write to %s", "sally")
 
-	// => Level: app.Dev Type: Error Time: 2015-02-01 12:32:40:4032 Func: TestDevLog Message: ...
+	// => Level: app.Dev Type: Error Time: 2015-02-01 12:32:40:4032 Func: TestDevLog  Line:30:322 Message: ...
 	Dev.Error(struct{}{}, "TestDevLog", "Error at procer", 100)
 	Dev.Errorf(struct{}{}, "TestDevLog", "Error at %d", 100)
 
-	// => Level: app.Dev Type: Info Time: 2015-02-01 12:32:40:4032 Func: TestDevLog Message: ...
+	// => Level: app.Dev Type: Info Time: 2015-02-01 12:32:40:4032 Func: TestDevLog  Line:30:322 Message: ...
 	Dev.Info(struct{}{}, "TestDevLog", "Write to work")
 	Dev.Infof(struct{}{}, "TestDevLog", "Write to %s", "slow")
 
 	//dumps the given data into json formatted output
-	// => Level: app.Dev Type: DataDump Time: 2015-02-01 12:32:40:4032 Func: TestDevLog Message: JSON Request Body...
+	// => Level: app.Dev Type: DataDump Time: 2015-02-01 12:32:40:4032 Func: TestDevLog Line:30:322 Message: JSON Request Body...
 	/* <DataDump
 
 		   map:
@@ -44,16 +44,16 @@ func TestUserLog(t *testing.T) {
 	User.Debug(struct{}{}, "TestUserLog", "Write to sumer")
 	User.Debugf(struct{}{}, "TestUserLog", "Write to %s", "sally")
 
-	// => Level: app.User Type: Error Time: 2015-02-01 12:32:40:4032 Func: TestUserLog Message: ...
+	// => Level: app.User Type: Error Time: 2015-02-01 12:32:40:4032 Message: ...
 	User.Error(struct{}{}, "TestUserLog", "Error at procer", 100)
 	User.Errorf(struct{}{}, "TestUserLog", "Error at %d", 100)
 
-	// => Level: app.User Type: Info Time: 2015-02-01 12:32:40:4032 Func: TestUserLog Message: ...
+	// => Level: app.User Type: Info Time: 2015-02-01 12:32:40:4032 Message: ...
 	User.Info(struct{}{}, "TestUserLog", "Write to work")
 	User.Infof(struct{}{}, "TestUserLog", "Write to %s", "slow")
 
 	//dumps the given data into json formatted output
-	// => Level: app.User Type: DataDump Time: 2015-02-01 12:32:40:4032 Func: TestUserLog Message:
+	// => Level: app.User Type: DataDump Time: 2015-02-01 12:32:40:4032 Message:
 	/* <DataDump
 
 		   map:
