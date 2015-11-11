83d5a1d0f058c6d087bf7a93006ceadbb4a9f2bb
Ewetumo Alexander <trinoxf@gmail.com>
2015-11-11 12:29:38 +0100

    updated conf package doc.go

--- conf/doc.go
+++ conf/doc.go
@@ -1,14 +1,17 @@
 // Package conf provides a package level configuration loader which loaders a
 // given set of configuration options using a given namespace and a map as the
 // storage endpoint.
-// To load a your configuration from your environment using a namespace with the
-// following all set within the environment
 //		os.Setenv("MYAPP_PROC_ID", "322")
 //		os.Setenv("MYAPP_SOCKET", "./tmp/sockets.po")
 //		os.Setenv("MYAPP_PORT", "4034")
+//		os.Setenv("MYAPP_Stamp", "2013-10-03 10:43:32.200")
+// To load the set of configuration values from your environment, simple do:
 //
-// Simple Do
-//      config := make(map[string]interface{})
-//      err := EnvConfig("myapp",config)
+//      err := Init("myapp")
 //
+// To Retrieve keys:
+//
+//  porc, found := conf.GetString("proc_id")
+//  port, foundAndParsed := conf.GetInt("port")
+//  ms, foundAndParsed := conf.GetTime("stamp")
 package conf
